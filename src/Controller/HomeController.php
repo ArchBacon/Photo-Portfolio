7d479323bca39c145624c0806d99cf0b10fe6a1b
ArchBacon <C.Delicaat@hotmail.com>
2022-04-04 22:17:28 +0200

    Used shifted template for homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -10,39 +10,9 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'home_spaced')]
+    #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('home/spaced.html.twig');
-    }
-
-    #[Route('/shifted', name: 'home_shifted')]
-    public function shifted(): Response
-    {
         return $this->render('home/shifted.html.twig');
-    }
-
-    #[Route('/spaced', name: 'home_spaced_multi')]
-    public function spaced(): Response
-    {
-        return $this->render('home/spaced-3-items.html.twig');
-    }
-
-    #[Route('/right', name: 'home_aligned_right')]
-    public function right(): Response
-    {
-        return $this->render('home/aligned-right.html.twig');
-    }
-
-    #[Route('/left', name: 'home_aligned_left')]
-    public function left(): Response
-    {
-        return $this->render('home/aligned-left.html.twig');
-    }
-
-    #[Route('/centered', name: 'home_centered')]
-    public function centered(): Response
-    {
-        return $this->render('home/centered.html.twig');
     }
 }
