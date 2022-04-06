e2fb33ee2b36292b050a5a995e6e1f37baa7153d
ArchBacon <C.Delicaat@hotmail.com>
2022-04-07 00:00:51 +0200

    Fixed some styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,14 @@ module.exports = {
             boxShadow: {
                 'darker': '0 2px 3px rgb(0 0 0 / 20%)',
                 'label': '1px 1px 1px rgb(0 0 0 / 10%), 5px 0 5px -3px rgb(0 0 0 / 40%), inset 0 0 5px rgb(0 0 0 / 4%)',
-            }
+                'mason': '5px 5px 10px 0 rgba(0, 0, 0, .8)',
+            },
+            gridTemplateColumns: {
+                'mason': 'repeat(auto-fill, minmax(180px, 1fr))',
+            },
+            gridAutoRows: {
+                'mason': 'minmax(180px, auto)',
+            },
         },
     },
     plugins: [],
