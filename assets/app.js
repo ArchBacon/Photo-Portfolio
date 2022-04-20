14174c500b5c16d1aa24e04a3107fc87cc2772df
ArchBacon <C.Delicaat@hotmail.com>
2022-04-20 23:12:13 +0200

    Added basic sidebar with overlay

--- assets/app.js
+++ assets/app.js
@@ -10,3 +10,39 @@ import './styles/app.css';
 
 // start the Stimulus application
 import './bootstrap';
+
+let overlay = document.getElementById('overlay');
+let sidebar = document.getElementById('sidebar');
+let sidebarClose = document.getElementById('sidebar-close');
+let sidebarOpen = document.getElementById('sidebar-open');
+
+function showOverlay() {
+    overlay.classList.replace('opacity-0', 'opacity-75');
+    overlay.classList.replace('pointer-events-none', 'pointer-events-auto');
+}
+
+function hideOverlay() {
+    overlay.classList.replace('opacity-75', 'opacity-0');
+    overlay.classList.replace('pointer-events-auto', 'pointer-events-none');
+}
+
+function showSidebar() {
+    sidebar.classList.replace('-right-96', 'right-0');
+    sidebarClose.classList.replace('pointer-events-none', 'pointer-events-auto');
+    sidebarClose.classList.replace('opacity-0', 'opacity-100');
+}
+
+function hideSidebar() {
+    sidebar.classList.replace('right-0', '-right-96');
+    sidebarClose.classList.replace('pointer-events-auto', 'pointer-events-none');
+    sidebarClose.classList.replace('opacity-100', 'opacity-0');
+}
+
+overlay.addEventListener("click", hideOverlay);
+overlay.addEventListener("click", hideSidebar);
+
+sidebarClose.addEventListener("click", hideOverlay);
+sidebarClose.addEventListener("click", hideSidebar);
+
+sidebarOpen.addEventListener("click", showOverlay);
+sidebarOpen.addEventListener("click", showSidebar);
