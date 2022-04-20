879450fc0b0f097ad053694ae27a0f6001ba8420
ArchBacon <C.Delicaat@hotmail.com>
2022-04-21 01:16:02 +0200

    Updated styling sidebar to make it responsive

--- assets/app.js
+++ assets/app.js
@@ -17,23 +17,25 @@ let sidebarClose = document.getElementById('sidebar-close');
 let sidebarOpen = document.getElementById('sidebar-open');
 
 function showOverlay() {
-    overlay.classList.replace('opacity-0', 'opacity-75');
+    overlay.classList.replace('opacity-0', 'opacity-90');
     overlay.classList.replace('pointer-events-none', 'pointer-events-auto');
 }
 
 function hideOverlay() {
-    overlay.classList.replace('opacity-75', 'opacity-0');
+    overlay.classList.replace('opacity-90', 'opacity-0');
     overlay.classList.replace('pointer-events-auto', 'pointer-events-none');
 }
 
 function showSidebar() {
-    sidebar.classList.replace('-right-96', 'right-0');
+    sidebar.classList.replace('-right-5/6', 'right-0');
+    sidebar.classList.replace('md:-right-96', 'md:right-0');
     sidebarClose.classList.replace('pointer-events-none', 'pointer-events-auto');
     sidebarClose.classList.replace('opacity-0', 'opacity-100');
 }
 
 function hideSidebar() {
-    sidebar.classList.replace('right-0', '-right-96');
+    sidebar.classList.replace('right-0', '-right-5/6');
+    sidebar.classList.replace('md:right-0', 'md:-right-96');
     sidebarClose.classList.replace('pointer-events-auto', 'pointer-events-none');
     sidebarClose.classList.replace('opacity-100', 'opacity-0');
 }
