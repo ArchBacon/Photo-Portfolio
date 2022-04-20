9526d50d0d6c383635ca24d747f3c2f4cbb4b8a9
ArchBacon <C.Delicaat@hotmail.com>
2022-04-20 15:34:16 +0200

    Basic setup scrollable tag (album) list

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,9 @@ module.exports = {
             'montserrat': ['"Montserrat"', 'sans-serif'],
         },
         extend: {
+            flex: {
+                '0': '0 0 auto'
+            },
             boxShadow: {
                 'darker': '0 2px 3px rgb(0 0 0 / 20%)',
                 'label': '1px 1px 1px rgb(0 0 0 / 10%), 5px 0 5px -3px rgb(0 0 0 / 40%), inset 0 0 5px rgb(0 0 0 / 4%)',
