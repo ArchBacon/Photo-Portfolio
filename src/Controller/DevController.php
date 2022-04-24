9e9b75e3631d451d34d4b20aee6af43e69254d09
ArchBacon <C.Delicaat@hotmail.com>
2022-04-25 00:02:02 +0200

    Added dev page for testing

---
+++ src/Controller/DevController.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DevController extends AbstractController
+{
+    #[Route('/dev', name: 'app_dev')]
+    public function index(): Response
+    {
+        return $this->render('dev/index.html.twig', [
+            'controller_name' => 'DevController',
+        ]);
+    }
+}
